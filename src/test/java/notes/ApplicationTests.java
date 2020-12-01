3b04463a76e359e8531739cb9c7972d4a8f6f3dc
barely-a-developer <19259418+barely-a-developer@users.noreply.github.com>
2020-11-30 21:29:26 -0500

    No tests

--- src/test/java/notes/ApplicationTests.java
+++ src/test/java/notes/ApplicationTests.java
@@ -1,12 +1,8 @@
 package notes;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
